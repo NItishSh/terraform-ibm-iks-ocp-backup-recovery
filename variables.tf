836045b983a20d80bc3550c5a1e7cc24541683d7
Nitish S Hanchinagoudra <Nitish.S.Hanchinagoudra@ibm.com>
2025-12-19 18:28:43 +0530

    fix: enable_auto_protect is disabled by default.

--- variables.tf
+++ variables.tf
@@ -260,5 +260,5 @@ variable "policy" {
 variable "enable_auto_protect" {
   type        = bool
   description = "Set to `true` to enable auto-protect on the registered cluster with the specified protection policy."
-  default     = true
+  default     = false
 }
