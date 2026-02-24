1b3f47a357c8e44a193f4d38ccb3204e091ad5e2
Nitish S Hanchinagoudra <Nitish.S.Hanchinagoudra@ibm.com>
2026-02-24 23:46:06 +0530

    feat: Update backup recovery module to v1.7.1 and modify test resource group name.

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "backup_recovery_instance" {
   source                    = "terraform-ibm-modules/backup-recovery/ibm"
-  version                   = "v1.7.0"
+  version                   = "v1.7.1"
   region                    = var.region
   resource_group_id         = var.cluster_resource_group_id
   ibmcloud_api_key          = var.ibmcloud_api_key
